db193af22cefdb411cba19889cb2584719335143
Kurilenko Aleksandr <ninznak@gmail.com>
2022-10-16 22:50:18 +0300

    все работает

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model() 
 
+
 class Group (models.Model):
     title = models.CharField('Название группы', max_length=200)
-    slug = models.SlugField('Описание', unique=True)
+    slug = models.SlugField('Краткое описание', unique=True)
     description = models.TextField('Описание')
 
     def __str__(self):
         return self.title
+        
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField('Текст поста')
+    pub_date = models.DateTimeField('Автор', auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -23,5 +27,6 @@ class Post(models.Model):
         Group,
         blank = True,
         null = True,
-        on_delete = models.CASCADE
+        on_delete = models.SET_NULL,
+        related_name='posts'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
 from django.http import HttpResponse
 
 
@@ -8,24 +10,27 @@ def index(request):
     text = 'Это главная страница проекта Yatube :))'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title' : title,
-        'text' : text,
-        'posts' : posts,
+        'title': title,
+        'text': text,
+        'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
+
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Лев Толстой – зеркало русской революции.'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    
     context = {
-        'title' : title,
-        'text' : text,
+        'group': group,
+        'posts': posts,        
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
+
 
 def name(request):
     return HttpResponse('<h1 style="color: red; font-size: 45px; margin: 20%;">Привет <i><b>путник!</b></i>')
+
 
 def yandex(request):
     return HttpResponse("<a href='https://yandex.ru/'>Ссылка на <b>Яндекс</b></a>")
